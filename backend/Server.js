236ea0c9782b5605f67a29abbdbf181971a45029
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-14 10:00:33 +0500

    succesfully run first server

--- backend/Server.js
+++ backend/Server.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const app = express();
 const PORT = 5000;
-// app.listen(PORT , console.log(`Backend is running on port : ${PORT}`));
-app.get('/' , (req, res) => res.json({msg: "Hello World!"}));
+app.listen(PORT , console.log(`Backend is running on port : ${PORT}`));
+app.get('/' , (req, res) => res.json({msg: "Hello World!"}))
