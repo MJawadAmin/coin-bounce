26af239ad60b430fc50dbdfe07a21c3f0797615d
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-14 17:34:57 +0500

    updates

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const dbConnect = require ("./database/db");
 const {PORT} = require("./config/index");
+const router = require("./routes/index");
 const app = express();
+app.use(router);
 dbConnect ();
 app.get('/', (req , res)=> res.json({msg : "Hello World"}) );
 app.listen(PORT, console.log(`Backend is running on port ${PORT}`));
