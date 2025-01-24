b501c38a99876fd3a128a0db85f189a0ba220263
Jaddikhan <114812094+Jaddikhan@users.noreply.github.com>
2025-01-25 01:23:17 +0500

    commits added

--- backend/Server.js
+++ backend/Server.js
@@ -1,21 +1,8 @@
-const express = require("express");
-const dbConnect = require("./database/db");
-const { PORT } = require("./config/index");
-const router = require("./routes/index");
-const errorHandler = require("./middleware/errorHandling"); // Corrected the folder name
-const app = express();
+import express from 'express'
+const app = express()
 
-// Middleware to parse JSON requests
-app.use(express.json());
+const PORT = 8080;
 
-// Connect to the database
-dbConnect();
+app.get('/', (req , res )=> res.json({msg : "Hello "}))
 
-// Use the routes
-app.use(router);
-
-// Error handling middleware (should be after routes)
-app.use(errorHandler);
-
-// Start the server
-app.listen(PORT, () => console.log(`Backend is running on port : ${PORT}`));
+app.listen(PORT , ()=>(console.log(`Sever are connected at ${PORT}`)))
